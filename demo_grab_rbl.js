c5f0d70fcce285e2f6cf2971eee6a95aaae76d56
Manu Vanneste <manu@lyke.be>
2026-06-26 16:07:45 +0200

    Update demo_grab_rbl.js

--- demo_grab_rbl.js
+++ demo_grab_rbl.js
@@ -1,11 +1,11 @@
-loadJS(`https://cdn.jsdelivr.net/gh/manulykebe/mockup-studio@main/grab_record_browser_list.js?_now=${Date.now()}`)
-.then(
-     () => {
-        const rbl=document.getElementsByClassName('record-browser-list')[0]
-        console.save(
-            grabRecordBrowserResultToCsv(
-                await grabRecordBrowserList(rbl)
-            ), 'export-' + document.URL + '.json'
-        )
-    }
-)
+if (typeof window !== 'undefined') {
+    loadJS(`https://cdn.jsdelivr.net/gh/manulykebe/mockup-studio@main/grab_record_browser_list.js?_now=${Date.now()}`)
+        .then(async () => {
+            const rbl = document.getElementsByClassName('record-browser-list')[0]
+            console.save(
+                grabRecordBrowserResultToCsv(
+                    await grabRecordBrowserList(rbl)
+                ), 'export-' + document.URL + '.json'
+            )
+        })
+}
